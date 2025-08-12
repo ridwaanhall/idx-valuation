ac85350d44fe46e2b68deb61c1f634be95faf811
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-08-12 18:24:26 +0000

    Add initial implementation of Stock Valuation Analyzer package

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Stock Valuation Analyzer Package
+
+A comprehensive tool for evaluating stock valuations using financial ratios.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Ridwan Halim"
+__all__ = [
+    "StockAnalyzer",
+    "FinancialRatios",
+    "ValuationModels"
+]
